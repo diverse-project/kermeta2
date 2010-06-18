70dad01c581c5b2e72444decbb859d5d604756df
Francois Fouquet <francoisfouquet@users.gforge.inria.fr>
2010-06-18 07:35:52 +0000

    Scala KMT Parser merge to branch V2

---
+++ org.kermeta.language.loader.scalaKMT/org.kermeta.scala.parser/src/main/scala/org/kermeta/scala/parser/Runner.scala
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.kermeta.scala.parser
+
+import scala.io.Source
+
+object Runner{
+
+  def main(args: Array[String]): Unit = {
+    val input = Source.fromFile(new java.io.File("sample.kmt")).getLines("\n").reduceLeft[String](_ + '\n' + _)
+
+    new Parser().lex(input)
+
+    var result = new Parser().parse(input)
+    result match {
+      case Some(_ @ res) => LoadSaveKM.saveKmModelingUnit("parsed.km", res) //new Interpreter(res).run
+      case None =>
+    }
+  }
+}
