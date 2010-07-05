8012d174cf0a51dd9f08c6232d570f4c6c62f644
Francois Fouquet <francoisfouquet@users.gforge.inria.fr>
2010-07-05 07:30:46 +0000

    Project creation

---
+++ org.kermeta.language.loader.emftextKMT/src/main/java/org/kermeta/language/loader/kmt/emftext/EMFTextKMTParser.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.kermeta.language.loader.kmt.emftext;
+
+import org.kermeta.language.api.port.PortResourceLoader;
+import org.kermeta.language.structure.ModelingUnit;
+
+/**
+ *
+ * @author ffouquet
+ */
+public class EMFTextKMTParser implements PortResourceLoader {
+
+    public ModelingUnit load(String uri, URIType type) {
+        return null;
+    }
+
+}
