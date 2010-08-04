137f6997e664ddfead04ba9f027ba84e6d68049b
hrambelo <hrambelo@users.gforge.inria.fr>
2010-08-04 15:02:44 +0000

    add component art2

--- org.kermeta.language.loader.scalaKMT/src/main/scala/org/kermeta/scala/parser/art2/impl/Art2ComponentLoader.java
+++ org.kermeta.language.loader.scalaKMT/src/main/scala/org/kermeta/scala/parser/art2/impl/Art2ComponentLoader.java
@@ -4,6 +4,12 @@
  */
 package org.kermeta.scala.parser.art2.impl;
 
+import org.kermeta.art2.annotation.ComponentType;
+import org.kermeta.art2.annotation.Port;
+import org.kermeta.art2.annotation.ProvidedPort;
+import org.kermeta.art2.annotation.Provides;
+import org.kermeta.art2.framework.AbstractComponentType;
+import org.kermeta.language.api.port.PortResourceLoader;
 import org.kermeta.language.structure.ModelingUnit;
 import org.kermeta.scala.parser.KParser;
 import org.kermeta.scala.parser.ParserUtil;
@@ -12,10 +18,17 @@ import org.kermeta.scala.parser.ParserUtil;
  *
  * @author ffouquet
  */
-public class Art2ComponentLoader implements org.kermeta.language.api.port.PortResourceLoader {
 
-    @Override
-    public ModelingUnit load(String uri, org.kermeta.language.api.port.PortResourceLoader.URIType type) {
+@Provides({
+	@ProvidedPort(name="KMTloader", className=PortResourceLoader.class)
+	}		
+)
+
+@ComponentType(libName="KMTloaderComponent")
+public class Art2ComponentLoader extends AbstractComponentType {//implements org.kermeta.language.api.port.PortResourceLoader {
+
+    @Port(name="KMTloader", method="load")
+    public ModelingUnit loadResource(String uri, org.kermeta.language.api.port.PortResourceLoader.URIType type) {
 
         KParser parser = new KParser();
 
