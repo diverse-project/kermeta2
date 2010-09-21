44b766c0cc7dbfc963ae095f5ee52ff5dc2de702
hrambelo <hrambelo@users.gforge.inria.fr>
2010-09-21 16:04:06 +0000

    add PortLexer port

--- org.kermeta.language.loader.scalaKMT/src/main/scala/org/kermeta/scala/parser/art2/impl/Art2ComponentLoader.java
+++ org.kermeta.language.loader.scalaKMT/src/main/scala/org/kermeta/scala/parser/art2/impl/Art2ComponentLoader.java
@@ -14,10 +14,15 @@ import org.kermeta.art2.annotation.Port;
 import org.kermeta.art2.annotation.ProvidedPort;
 import org.kermeta.art2.annotation.Provides;
 import org.kermeta.art2.framework.AbstractComponentType;
+import org.kermeta.language.api.ktoken.IKToken;
+import org.kermeta.language.api.port.PortLexer;
 import org.kermeta.language.api.port.PortResourceLoader;
 import org.kermeta.language.structure.ModelingUnit;
 import org.kermeta.scala.parser.KParser;
 import org.kermeta.scala.parser.ParserUtil;
+import org.kermeta.language.lexer.KMLexer;
+
+import scala.util.parsing.combinator.token.Tokens.Token;
 
 /**
  *
@@ -25,19 +30,35 @@ import org.kermeta.scala.parser.ParserUtil;
  */
 
 @Provides({
-	@ProvidedPort(name="KMTloader", className=PortResourceLoader.class)
+	@ProvidedPort(name="KMTloader", className=PortResourceLoader.class),
+	@ProvidedPort(name="KMTlexer", className=PortLexer.class)
 	}		
 )
 
 @ComponentType(libName="KMTloaderComponent")
 public class Art2ComponentLoader extends AbstractComponentType implements org.kermeta.language.api.port.PortResourceLoader {
-
+	
     @Port(name="KMTloader", method="load")
     public ModelingUnit load(String uri, org.kermeta.language.api.port.PortResourceLoader.URIType type) {
 
         KParser parser = new KParser();
 
         return parser.parseSynch(ParserUtil.loadFile(uri)).get();
+
+    }
+    
+    @Port(name="KMTlexer", method="lex")
+    public IKToken lex(String content) {
+    	org.kermeta.language.api.ktoken.IKToken actualToken = null;
+        KMLexer lexer = new KMLexer(content);
+        try{
+			Object t = lexer.nextToken();
+			if(t instanceof org.kermeta.language.api.ktoken.IKToken){
+				actualToken = ( org.kermeta.language.api.ktoken.IKToken) t;
+			}
+		} catch (Exception e){
+		}
+        return actualToken;
 
     }
 }
