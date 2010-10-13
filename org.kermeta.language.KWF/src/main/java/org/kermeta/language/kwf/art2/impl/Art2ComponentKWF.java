6a9ec7664c442440fb9c695036ec8037b6c50803
hrambelo <hrambelo@users.gforge.inria.fr>
2010-10-13 16:58:28 +0000

    update Log Port from PortLog to MessagePort for broadcasting update pom parent to 2.0.1-SNAPSHOT

--- org.kermeta.language.KWF/src/main/java/org/kermeta/language/kwf/art2/impl/Art2ComponentKWF.java
+++ org.kermeta.language.KWF/src/main/java/org/kermeta/language/kwf/art2/impl/Art2ComponentKWF.java
@@ -11,6 +11,7 @@
 package org.kermeta.language.kwf.art2.impl;
 
 import org.kermeta.art2.framework.AbstractComponentType;
+import org.kermeta.art2.framework.MessagePort;
 import org.kermeta.art2.annotation.ComponentType;
 import org.kermeta.art2.annotation.Port;
 import org.kermeta.art2.annotation.PortType;
@@ -23,7 +24,6 @@ import org.kermeta.art2.annotation.Stop;
 import org.kermeta.language.api.kevent.KEvent;
 import org.kermeta.language.api.messaging.UnifiedMessageFactory;
 import org.kermeta.language.api.port.PortKEvent;
-import org.kermeta.language.api.port.PortLog;
 import org.osgi.framework.Bundle;
 
 
@@ -44,16 +44,16 @@ import org.osgi.framework.Bundle;
 	/**
 	 * Log port for sending technical messages
 	 */
-	@RequiredPort(name = "log", type=PortType.SERVICE, className=PortLog.class)
+	@RequiredPort(name = "log", type=PortType.MESSAGE)
 })
 
 
-@ComponentType(libName = "org.kermeta.language2")
+@ComponentType(libName = "org.kermeta.language")
 public class Art2ComponentKWF extends AbstractComponentType  implements org.kermeta.language.api.port.PortKEvent{
 	
 	protected String bundleSymbolicName="";
 	protected Bundle bundle;
-	protected PortLog logPort=null;
+	protected MessagePort logPort=null;
 	protected UnifiedMessageFactory mFactory = UnifiedMessageFactory.getInstance();
 	
 	/**
@@ -74,11 +74,11 @@ public class Art2ComponentKWF extends AbstractComponentType  implements org.kerm
 	@Start
 	public void start(){
 		instance =  this;
-		logPort = getPortByName("log", PortLog.class);
+		logPort = getPortByName("log", MessagePort.class);
 		bundle = (Bundle)this.getDictionary().get("osgi.bundle");
 		bundleSymbolicName = bundle.getHeaders().get("Bundle-SymbolicName").toString();
 		System.out.println("Successfully started KWF");
-		logPort.log(mFactory.createDebugMessage("Successfully started KWF" , bundleSymbolicName));
+		logPort.process(mFactory.createDebugMessage("Successfully started KWF" , bundleSymbolicName));
 	}
 	
 	@Stop
@@ -89,7 +89,7 @@ public class Art2ComponentKWF extends AbstractComponentType  implements org.kerm
 	@Port(name="kevent", method="processKEvent")
 	public void processKEvent(KEvent event) {
 		//log the incoming event
-		logPort.log(mFactory.createInfoMessage("KWF received new event : " + event, bundleSymbolicName));
+		logPort.process(mFactory.createInfoMessage("KWF received new event : " + event, bundleSymbolicName));
 		//Advanced event processing goes here : define action according to event type
 		
 	}
