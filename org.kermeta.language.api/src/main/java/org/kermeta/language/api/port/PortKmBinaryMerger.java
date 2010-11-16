589bcfd57acbb6f4d1b13caaaf751709096619b4
Didier Vojtisek <didier.vojtisek@inria.fr>
2010-11-16 10:36:08 +0000

    rename merger port into binary merger

---
+++ org.kermeta.language.api/src/main/java/org/kermeta/language/api/port/PortKmBinaryMerger.java
@@ -0,0 +1,21 @@
+/* $Id:$ 
+ * Creation : November, 2010
+ * Licence  : EPL 
+ * Copyright: IRISA/INRIA
+ * Authors  : 
+ *            Didier Vojtisek
+ */
+
+package org.kermeta.language.api.port;
+
+import java.util.Collection;
+
+import org.kermeta.language.structure.ModelingUnit;
+
+/**
+ * This service offers to merge 2 ModelingUnits into a single one
+ */
+public interface PortKmBinaryMerger {
+
+    public ModelingUnit merge(ModelingUnit first_mu,ModelingUnit second_mu, Collection<String> directives);
+}
