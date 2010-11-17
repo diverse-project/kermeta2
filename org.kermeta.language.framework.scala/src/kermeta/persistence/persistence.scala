0631701ea2b957b07bbd252814468b82576c6c51
Francois Fouquet <francoisfouquet@users.gforge.inria.fr>
2010-11-17 08:34:37 +0000

    Fix reflexivity loader

--- org.kermeta.language.framework.scala/src/kermeta/persistence/persistence.scala
+++ org.kermeta.language.framework.scala/src/kermeta/persistence/persistence.scala
@@ -68,6 +68,7 @@ class EMFRepository  extends Repository  {
      
     def registerEcoreFile(mm_uri : String)={mm_uri}
     def getResource(uri : String) :Resource={
+      println("GetRessource URI="+uri)
         var res : EMFResource = new EMFResource()
         res.setResourceSet(rs)
         var uri1 :URI = URI.createURI(uri);
