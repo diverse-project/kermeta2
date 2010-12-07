f4f5f31f7eb50a535670c3603f9a15d6aaf3a255
Olivier Barais <barais@irisa.fr>
2010-12-07 23:06:35 +0000

    Update dependancies

--- fr.irisa.triskell.kermeta.scala.compilo/src/main/scala/fr/irisa/triskell/embedded/POMGeneratorHelper.scala
+++ fr.irisa.triskell.kermeta.scala.compilo/src/main/scala/fr/irisa/triskell/embedded/POMGeneratorHelper.scala
@@ -233,10 +233,10 @@ object POMGeneratorHelper {
     var dependencies = new scala.collection.mutable.ArrayBuffer[Dependency]
     
 
-    dependencies.add(this.createDependency("org.scala-lang", "scala-library", "2.8.0"))
+    dependencies.add(this.createDependency("org.scala-lang", "scala-library", "2.8.1"))
     dependencies.add(createDependency("org.kermeta", "kermeta.model", "1.3.0"));
     dependencies.add(createDependency("org.kermeta.emf", "emf.lib", "2.6.0"));
-    dependencies.add(createDependency("org.kermeta.language", "language.framework.scala", "0.0.6"));
+    dependencies.add(createDependency("org.kermeta.language", "language.framework.scala", "0.0.8"));
     /* INIT LOCAL VARIABLE DEPENDENCY */
 
     var i = 0
