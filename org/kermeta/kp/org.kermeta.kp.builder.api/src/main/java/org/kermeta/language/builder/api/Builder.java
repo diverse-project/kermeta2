83606f0ba1a1a23c110b6c12914d81b52ff15618
Didier Vojtisek <didier.vojtisek@inria.fr>
2013-02-27 15:53:49 +0100

    moved project to kp

---
+++ org/kermeta/kp/org.kermeta.kp.builder.api/src/main/java/org/kermeta/language/builder/api/Builder.java
@@ -0,0 +1,26 @@
+package org.kermeta.language.builder.api;
+
+import java.util.ArrayList;
+
+import org.eclipse.core.resources.IResource;
+
+public abstract class Builder {
+	
+	protected Builder(){
+		super();
+	}
+
+	public final static String LOG_MESSAGE_GROUP = "org.kermeta.language.builder";
+	
+	public abstract void parseSpecificFile(IResource toParse, String content);
+	public abstract void runFromKP(String kpIdentifier,ArrayList<String> params);
+	public abstract void runFromKP(String kpIdentifier, String mainClass, String mainOperation, ArrayList<String> params);
+	public abstract void buildFromKP(String kpIdentifier);
+	public abstract void compileFromKP(String kpIdentifier);
+	public abstract void compileFromKP(String kpIdentifier, boolean checkEnabled);
+	public abstract String findKPidentifierFromKMT(IResource kmt);
+	public abstract void setDirty(IResource kmt, boolean dirty);
+	
+	public abstract String generateIdentifier(IResource element);
+
+}
