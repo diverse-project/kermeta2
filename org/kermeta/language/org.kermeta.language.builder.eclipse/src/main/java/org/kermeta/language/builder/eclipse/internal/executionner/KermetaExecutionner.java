e984faba8d7ba04323131ce5175236b5ff390a55
Didier Vojtisek <didier.vojtisek@inria.fr>
2013-02-27 15:55:47 +0100

    continuing artifactId refactoring

--- org/kermeta/language/org.kermeta.language.builder.eclipse/src/main/java/org/kermeta/language/builder/eclipse/internal/executionner/KermetaExecutionner.java
+++
@@ -1,9 +0,0 @@
-package org.kermeta.language.builder.eclipse.internal.executionner;
-
-import org.eclipse.core.runtime.IProgressMonitor;
-
-public abstract class KermetaExecutionner<G,H>{
-	
-	public abstract void execute(G concernedResource, H content, IProgressMonitor monitor);
-
-}
