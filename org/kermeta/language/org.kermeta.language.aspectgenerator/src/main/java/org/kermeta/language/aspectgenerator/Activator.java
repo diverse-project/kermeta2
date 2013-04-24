f7f3aabca3251c37053767558a386a40be20428a
Didier Vojtisek <didier.vojtisek@inria.fr>
2013-04-24 11:12:23 +0200

    adding missing activator file

---
+++ org/kermeta/language/org.kermeta.language.aspectgenerator/src/main/java/org/kermeta/language/aspectgenerator/Activator.java
@@ -0,0 +1,30 @@
+package org.kermeta.language.aspectgenerator;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+import org.kermeta.language.aspectgenerator.impl.KM2KMTAspectGeneratorImpl4Eclipse;;
+
+
+public class Activator implements BundleActivator{
+
+	
+	ServiceRegistration serviceRegistration;
+	@Override
+	public void start(BundleContext ctx) throws Exception {
+		//property for the service
+		Dictionary<String, ? extends Object> args = new Hashtable<String,  Object>() ;
+		serviceRegistration = ctx.registerService(KM2KMTAspectGenerator.class.getName(), new KM2KMTAspectGeneratorImpl4Eclipse(), args);
+	}
+
+	@Override
+	public void stop(BundleContext ctx) throws Exception {
+		if (serviceRegistration!=null)
+			serviceRegistration.unregister();
+		
+	}
+
+}
