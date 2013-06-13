23bb032f35e213d96601e466b4cc808d70837a01
Fabien Coulon <fabien.coulon@inria.fr>
2013-06-13 16:07:37 +0200

    FSM sample: Add the example projects in the deployer plugin.
    Now user can create FSM projects in : File -> New -> Example and select
    Kermeta 2 FSM sample

---
+++ org/kermeta/language/sample/org.kermeta.language.sample.deployer/src/main/java/org/kermeta/language/sample/deployer/wizards/FsmExampleWizard.java
@@ -0,0 +1,30 @@
+package org.kermeta.language.sample.deployer.wizards;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.kermeta.language.sample.deployer.Activator;
+
+public class FsmExampleWizard extends AbstractExampleWizard {
+	
+	protected Collection<ProjectDescriptor> getProjectDescriptors() {		
+		// We need the statements example to be unzipped along with the
+		// EMF library example model, edit and editor examples
+		List<ProjectDescriptor> projects = new ArrayList<ProjectDescriptor>(5);
+		projects.add(new ProjectDescriptor("org.kermeta.language.sample.deployer", "zips/org.kermeta.language.sample.fsm.model.zip", 			"org.kermeta.language.sample.fsm.model"));
+		projects.add(new ProjectDescriptor("org.kermeta.language.sample.deployer", "zips/org.kermeta.language.sample.fsm.checker.zip", 			"org.kermeta.language.sample.fsm.checker"));
+		projects.add(new ProjectDescriptor("org.kermeta.language.sample.deployer", "zips/org.kermeta.language.sample.fsm.simulation.zip", 		"org.kermeta.language.sample.fsm.simulation"));
+		projects.add(new ProjectDescriptor("org.kermeta.language.sample.deployer", "zips/org.kermeta.language.sample.fsm.determinization.zip", 	"org.kermeta.language.sample.fsm.determinization"));
+		projects.add(new ProjectDescriptor("org.kermeta.language.sample.deployer", "zips/org.kermeta.language.sample.fsm.minimization.zip", 	"org.kermeta.language.sample.fsm.minimization"));
+		return projects;
+	}
+
+	@Override
+	protected AbstractUIPlugin getContainerPlugin() {
+		
+		return Activator.getDefault();
+	}
+
+}
