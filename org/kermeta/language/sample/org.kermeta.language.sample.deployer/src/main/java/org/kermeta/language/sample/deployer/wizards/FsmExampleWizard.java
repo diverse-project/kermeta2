78a315a0dabfff46cb676e77940fb056dcc2b414
Fabien Coulon <fabien.coulon@inria.fr>
2013-06-26 11:07:22 +0200

    FSM sample : Add org.kermeta.language.sample.fsm.extended in the deployer to be got with other projects of the FSM sample provided by Kermeta 2 workbench.

--- org/kermeta/language/sample/org.kermeta.language.sample.deployer/src/main/java/org/kermeta/language/sample/deployer/wizards/FsmExampleWizard.java
+++ org/kermeta/language/sample/org.kermeta.language.sample.deployer/src/main/java/org/kermeta/language/sample/deployer/wizards/FsmExampleWizard.java
@@ -14,6 +14,7 @@ public class FsmExampleWizard extends AbstractExampleWizard {
 		// EMF library example model, edit and editor examples
 		List<ProjectDescriptor> projects = new ArrayList<ProjectDescriptor>(5);
 		projects.add(new ProjectDescriptor("org.kermeta.language.sample.deployer", "zips/org.kermeta.language.sample.fsm.model.zip", 			"org.kermeta.language.sample.fsm.model"));
+		projects.add(new ProjectDescriptor("org.kermeta.language.sample.deployer", "zips/org.kermeta.language.sample.fsm.extended.zip", 		"org.kermeta.language.sample.fsm.extended"));
 		projects.add(new ProjectDescriptor("org.kermeta.language.sample.deployer", "zips/org.kermeta.language.sample.fsm.checker.zip", 			"org.kermeta.language.sample.fsm.checker"));
 		projects.add(new ProjectDescriptor("org.kermeta.language.sample.deployer", "zips/org.kermeta.language.sample.fsm.simulation.zip", 		"org.kermeta.language.sample.fsm.simulation"));
 		projects.add(new ProjectDescriptor("org.kermeta.language.sample.deployer", "zips/org.kermeta.language.sample.fsm.determinization.zip", 	"org.kermeta.language.sample.fsm.determinization"));
