8ac9256c9fcd45e42ce6c4fa4739cd2764e1d359
Didier Vojtisek <didier.vojtisek@inria.fr>
2013-07-24 14:57:38 +0200

    switching back to dev version (2.1.1-SNAPSHOT)

--- org/kermeta/kp/org.kermeta.kp.wizard.eclipse/src/org/kermeta/kp/wizard/eclipse/preferences/PreferenceInitializer.java
+++ org/kermeta/kp/org.kermeta.kp.wizard.eclipse/src/org/kermeta/kp/wizard/eclipse/preferences/PreferenceInitializer.java
@@ -32,7 +32,7 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 		        "\n\t}"+
 		        "\n}");
 		store.setDefault(PreferenceConstants.P_NEW_KP_TEMPLATE_STRING, 
-				"resource library_standard =\"platform:/plugin/org.kermeta.language.library.standard\" alternative \"mvn:org.kermeta.language/org.kermeta.language.library.standard/2.1.0\"\n"+
+				"resource library_standard =\"platform:/plugin/org.kermeta.language.library.standard\" alternative \"mvn:org.kermeta.language/org.kermeta.language.library.standard/2.1.1-SNAPSHOT\"\n"+
 				"importProject library_standard\n" +
 
 				"project ${project.name} {\n"+ 
